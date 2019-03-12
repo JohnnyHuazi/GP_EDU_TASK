74e42ab4bb980c7cb3947cecc083be4dc9da285b
ywh <673608242@qq.com>
2019-03-12 23:35:57 +0800

    the fist commit

---
+++ hnxc-gp-edu-task/src/main/java/com/hnxc/gp/edu/task/HnxcGpEduTaskApplication.java
@@ -0,0 +1,13 @@
+package com.hnxc.gp.edu.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnxcGpEduTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HnxcGpEduTaskApplication.class, args);
+	}
+
+}
